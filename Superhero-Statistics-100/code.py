035d6fcdedfaf4040012b043d7aefb02a388d701
lakshmi-sbabu <72311304+lakshmi-sbabu@users.noreply.github.com>
2021-01-22 21:17:00 -0500

    Code file created.

---
+++ Superhero-Statistics-100/code.py
@@ -0,0 +1,24 @@
+# --------------
+#Header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Reading of the file
+data=pd.read_csv(path)
+
+# Code starts here
+data['Gender'].replace('-','Agender',inplace = True)
+data['Gender'].value_counts().plot(kind='bar')
+data['Alignment'].value_counts().plot(kind = 'bar')
+data['Combat'].corr(data['Intelligence'])
+data['Combat'].corr(data['Strength'])
+data['Total'].quantile(.99)
+best = data[data['Total'] > 554.05]
+best.head()
+super_best_names = best['Name'].tolist()
+print(super_best_names)
+
+
+
